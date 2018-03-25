aee65a45898b40aa8749de85969e8ba506a22bc7
laibavi <017laibavi.wts@gmail.com>
2018-03-25 13:25:34 +0700

    change 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root "static_pages#home"
 
-  root "application#hello"
+  get "/help", to: "static_pages#help"
+  get "/home", to: "static_pages#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
