702c44e4177393f218a0e734774db83ef5499726
laibavi <017laibavi.wts@gmail.com>
2018-03-29 14:34:37 +0700

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get "users/new", to: "users#new"
 
   root "static_pages#home"
-
+  get "/signup", to: "users#new"
   get "/help", to: "static_pages#help"
   get "/home", to: "static_pages#home"
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
+  get  "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
 end
