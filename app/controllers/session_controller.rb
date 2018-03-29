564dc157dd2ef6f4c7de5b809d04bc66b3fd7cf3
laibavi <017laibavi.wts@gmail.com>
2018-03-29 14:34:37 +0700

    Chapter-9

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,10 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = t "notipass"
@@ -15,7 +16,8 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,8 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
-    params.require(:user).permit :name, :email, :password, :password_confirmation
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
-
 end
